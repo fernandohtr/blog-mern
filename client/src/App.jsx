dc51b1cf38ab6cf8d7b5a9210bbdacc605fc5a02
Fernando Toledo <fernandohtr@gmail.com>
2023-03-05 13:59:55 -0300

    Create main page blog

---
+++ client/src/App.jsx
@@ -0,0 +1,57 @@
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="" className="logo">Blog MERN</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+
+      <article>
+        <div className="image">
+          <img src="https://techcrunch.com/wp-content/uploads/2022/02/GettyImages-1125071391-e1677975112261.jpg?w=430&h=230&crop=1" alt="" />
+        </div>
+        <div className="text">
+          <h2>Amazon and Better.com’s unlikely pairing</h2>
+          <p className="info">
+            <a href="" className="author">Fernando Toledo</a>
+            <span className="time">2023-03-15</span>
+          </p>
+          <p className="summary">Welcome to The Interchange! If you received this in your inbox, thank you for signing up and your vote of confidence.</p>
+        </div>
+      </article>
+      <article>
+        <div className="image">
+          <img src="https://techcrunch.com/wp-content/uploads/2022/02/GettyImages-1125071391-e1677975112261.jpg?w=430&h=230&crop=1" alt="" />
+        </div>
+        <div className="text">
+          <h2>Amazon and Better.com’s unlikely pairing</h2>
+          <p className="info">
+            <a href="" className="author">Fernando Toledo</a>
+            <span className="time">2023-03-15</span>
+          </p>
+          <p className="summary">Welcome to The Interchange! If you received this in your inbox, thank you for signing up and your vote of confidence.</p>
+        </div>
+      </article>
+      <article>
+        <div className="image">
+          <img src="https://techcrunch.com/wp-content/uploads/2022/02/GettyImages-1125071391-e1677975112261.jpg?w=430&h=230&crop=1" alt="" />
+        </div>
+        <div className="text">
+          <h2>Amazon and Better.com’s unlikely pairing</h2>
+          <p className="info">
+            <a href="" className="author">Fernando Toledo</a>
+            <span className="time">2023-03-15</span>
+          </p>
+          <p className="summary">Welcome to The Interchange! If you received this in your inbox, thank you for signing up and your vote of confidence.</p>
+        </div>
+      </article>
+    </main>
+  );
+}
+
+export default App;
