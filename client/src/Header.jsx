5d3aef6bea4765069952e89122777fa19e5976b0
Fernando Toledo <fernandohtr@gmail.com>
2023-03-05 21:17:56 -0300

    Add react-router-dom and refact App.jsx

---
+++ client/src/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <header>
+      <a href="" className="logo">Blog MERN</a>
+      <nav>
+        <a href="">Login</a>
+        <a href="">Register</a>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
---
+++ client/src/Post.jsx
@@ -0,0 +1,19 @@
+function Post() {
+  return (
+    <article>
+      <div className="image">
+        <img src="https://techcrunch.com/wp-content/uploads/2022/02/GettyImages-1125071391-e1677975112261.jpg?w=430&h=230&crop=1" alt="" />
+      </div>
+      <div className="text">
+        <h2>Amazon and Better.com’s unlikely pairing</h2>
+        <p className="info">
+          <a href="" className="author">Fernando Toledo</a>
+          <span className="time">2023-03-15</span>
+        </p>
+        <p className="summary">Welcome to The Interchange! If you received this in your inbox, thank you for signing up and your vote of confidence.</p>
+      </div>
+    </article>
+  )
+}
+
+export default Post
