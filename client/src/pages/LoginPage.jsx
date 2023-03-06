b17e49c14e3a0f3ba7877155100b56fbc6b196f9
Fernando Toledo <fernandohtr@gmail.com>
2023-03-05 22:00:01 -0300

    Refact Login and Register pages

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,8 +1,11 @@
 function LoginPage() {
   return (
-    <>
-      Login Page
-    </>
+    <form className='login' action='' method='post'>
+      <h1>Login</h1>
+      <input type='text' name='' id='' placeholder='username' />
+      <input type='password' name='' id='' placeholder='password' />
+      <button type="submit">Login</button>
+    </form>
   )
 }
 
---
+++ client/src/pages/RegisterPage.jsx
@@ -0,0 +1,12 @@
+function RegisterPage() {
+  return (
+    <form className='register' action='' method='post'>
+      <h1>Register</h1>
+      <input type='text' name='' id='' placeholder='username' />
+      <input type='password' name='' id='' placeholder='password' />
+      <button type="submit">Register</button>
+    </form>
+  )
+}
+
+export default RegisterPage
