368eaa719a00265e22fc93581dae4de793930260
Fernando Toledo <fernandohtr@gmail.com>
2023-03-07 19:29:42 -0300

    Add useState at RegisterPager

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,9 +1,35 @@
+import { useState } from "react"
+
 function RegisterPage() {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  async function register(event) {
+    event.preventDefault()
+    await fetch('http://localhost:4000', {
+      method: 'POST',
+      body: JSON.stringify({username, password}),
+      headers: {'Content-type': 'applicartion/json'}
+    })
+  }
+
   return (
-    <form className='register' action='' method='post'>
+    <form className='register' action='' method='post' onSubmit={register}>
       <h1>Register</h1>
-      <input type='text' name='' id='' placeholder='username' />
-      <input type='password' name='' id='' placeholder='password' />
+      <input type='text'
+             name=''
+             id=''
+             placeholder='username'
+             value={username}
+             onChange={event => setUsername(event.target.value)}
+      />
+      <input type='password'
+             name=''
+             id=''
+             placeholder='password'
+             value={password}
+             onChange={event => setPassword(event.target.value)}
+      />
       <button type="submit">Register</button>
     </form>
   )
