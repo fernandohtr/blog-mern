9c0d04909641bbf81c1e5053aa6662a9e8f3266c
Fernando Toledo <fernandohtr@gmail.com>
2023-03-10 01:30:55 -0300

    Add jwt dependency and add login route

--- api/index.js
+++ api/index.js
@@ -2,15 +2,21 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const app = express();
 
 const User = require('./models/User');
 
 const salt = bcrypt.genSaltSync(10);
+const secret = '%2A1kQAd94nww@#Wqdj6ZNvWcn0SBDs@';
 
 require('dotenv').config()
 
-app.use(cors());
+app.use(cors({
+  credentials: true,
+  origin: 'http://localhost:3000',
+}));
 app.use(express.json());
 
 const url = `mongodb+srv://${process.env.MONGO_USER}:` +
@@ -27,9 +33,23 @@ app.post('/register', async (request, response) => {
       password: bcrypt.hashSync(password, salt),
     });
   } catch(err) {
-    response.status(409).json(err);
+    response.status(500).json({message: 'Registration failed'});
   }
 });
+
+app.post('/login', async (request, response) => {
+  const {username, password} = request.body;
+  const userData = await User.findOne({username})
+  const passwordOk = bcrypt.compareSync(password, userData.password)
+  if (passwordOk) {
+    jwt.sign({username, id: userData._id}, secret, {}, (err, token) => {
+      if (err) throw err;
+      response.cookie('token', token).json('ok');
+    });
+  } else {
+    response.status(401).json({message: 'Login failed'});
+  }
+})
 
 app.listen(4000, () => {
   console.log('Server running');
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,10 +1,48 @@
+import { useState } from 'react'
+import { Navigate } from 'react-router-dom'
+
 function LoginPage() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [redirect, setRedirect] = useState(false);
+
+  async function login(event) {
+    event.preventDefault();
+    const response = await fetch('http://localhost:4000/login', {
+      method: 'POST',
+      body: JSON.stringify({username, password}),
+      headers: {'Content-type': 'application/json'},
+      credentials: 'include',
+    })
+    if (response.ok) {
+      setRedirect(true);
+    } else {
+      alert('Login failed');
+    }
+  }
+
+  if (redirect) {
+    return <Navigate to={'/'} />
+  }
+  
   return (
-    <form className='login' action='' method='post'>
+    <form className='login' action='' method='post' onSubmit={login}>
       <h1>Login</h1>
-      <input type='text' name='' id='' placeholder='username' />
-      <input type='password' name='' id='' placeholder='password' />
-      <button type="submit">Login</button>
+      <input type='text'
+             name=''
+             id=''
+             placeholder='username'
+             value={username}
+             onChange={event => setUsername(event.target.value)}
+      />
+      <input type='password'
+             name=''
+             id=''
+             placeholder='password'
+             value={password}
+             onChange={event => setPassword(event.target.value)}
+      />
+      <button type='submit'>Login</button>
     </form>
   )
 }
